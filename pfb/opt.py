3bda75aac4125a3e6984fdaa2f7e3b0b0df4a2f4
landmanbester <lbester@ska.ac.za>
2020-04-05 16:03:14 +0200

    Added solve_x0 script for fine tuning

--- pfb/opt.py
+++ pfb/opt.py
@@ -2,8 +2,6 @@ import numpy as np
 from numba import njit, prange
 from scipy.linalg import norm, svd
 
-
-
 def pcg(A, b, x0, M=None, tol=1e-5, maxit=500, verbosity=1, report_freq=10):
     
     if M is None:
@@ -19,111 +17,42 @@ def pcg(A, b, x0, M=None, tol=1e-5, maxit=500, verbosity=1, report_freq=10):
         eps0 = rnorm
     k = 0
     x = x0
-    xbest = x
     while rnorm/eps0 > tol and k < maxit:
+        xp = x.copy()
+        rp = r.copy()
         Ap = A(p)
         rnorm = np.vdot(r, y)
         alpha = rnorm/np.vdot(p, Ap)
-        x += alpha*p
-        r += alpha*Ap
+        x = xp + alpha*p
+        r = rp + alpha*Ap
         y = M(r)
         rnorm_next = np.vdot(r, y)
+        while rnorm_next > rnorm:  # TODO - better line search
+            alpha *= 0.75
+            x = xp + alpha*p
+            r = rp + alpha*Ap
+            y = M(r)
+            rnorm_next = np.vdot(r, y)
+
         beta = rnorm_next/rnorm
         p = beta*p - y
         rnorm = rnorm_next
         k += 1
-        if k == 1:
-            kbest = k
-            epsbest = rnorm
-            xbest = x
-        elif rnorm < epsbest:
-            kbest = k
-            epsbest = rnorm
-            xbest = x
 
-        if not k%report_freq and verbosity >= 1:
+        if not k%report_freq and verbosity > 1:
             print("At iteration %i rnorm = %f"%(k, rnorm/eps0))
 
     if k >= maxit:
-        if verbosity >= 1:
-            print("CG - Returning best fit after %i iterations. Norm of residual = %f.  "%(kbest, epsbest/eps0))
+        if verbosity > 0:
+            print("CG - Maximum iterations reacged. Norm of residual = %f.  "%(rnorm/eps0))
     else:
-        if verbosity >= 1:
+        if verbosity > 0:
             print("CG - Success, converged after %i iterations"%k)
-    return xbest    
+    return x
 
 def back_track_func(x, xold, gradp, likp, L):
         df = x - xold
-        return likp + np.vdot(gradp, df) + L* np.vdot(df, df)/2 
-
-def solve_x0(A, b, x0, y0, L, sigma0, tol=1e-4, maxit=500, positivity=False, report_freq=10):
-    """
-    Proximal gradient algorithm to generate initial soln of
-
-    argmin_x x.T(2 * ID - Ax) + x.T K^{-1} x s.t. x >= 0 
-
-    A - psf.convolve
-    ID - dirty image
-    x0 - initial guess
-    L - max eigenvalue of A.T A
-    K - prior covariance matrix
-
-    """
-    x = x0.copy()
-    y = y0.copy()
-    eps = 1.0
-    k = 0
-    t = 1.0
-    gradn = A(x) - b
-    likn = np.vdot(x, gradn) - np.vdot(x, b)
-    while eps > tol and k < maxit:
-        # gradient decent update
-        xold = x.copy()
-        gradp = gradn
-        likp = likn
-        
-        # compute prox with line search
-        tmp = y - gradp/L 
-        if positivity:
-            tmp[tmp<0.0] = 0.0
-        x = tmp/(1 + sigma0/L)
-        gradn = A(x) - b 
-        likn = np.vdot(x, gradn) - np.vdot(x, b)
-        flam = back_track_func(x, xold, gradp, likp, L)
-        # if likn > flam:
-        while likn > flam:
-            print("Step size too large, adjusting L", L)
-            L *= 1.5
-            tmp = y - gradp/L 
-            if positivity:
-                tmp[tmp<0.0] = 0.0
-            x = tmp/(1 + sigma0/L)
-            gradn = A(x) - b 
-            likn = np.vdot(x, gradn) - np.vdot(x, b)
-            flam = back_track_func(x, xold, gradp, likp, L)
-        # else:
-        #     L /= 1.2
-
-        # fista update
-        tp = t
-        t = (1. + np.sqrt(1. + 4 * tp**2))/2.
-
-        # soln update
-        y = x + (tp - 1)/t * (x - xold)
-        
-        # check convergence criteria
-        eps = norm(x - xold)/norm(x)
-
-        if not k%report_freq:
-            print("At iteration %i eps = %f and current stepsize is %f"%(k, eps, L))
-
-        k += 1
-
-    if k >= maxit:
-        print("PG - Maximum iterations reached. Relative difference between updates = ", eps)
-    else:
-        print("PG - Success, converged after %i iterations"%k)
-    return x, y, L
+        return likp + np.vdot(gradp, df) + L* np.vdot(df, df)/2
 
 def fista(fprime,
           x0, y0,  # initial guess for primal and dual variables 
@@ -239,95 +168,117 @@ def fista(fprime,
     return x, objective, fidelity, fidupper
 
 
-def hpd(A, b, 
-        x0, v210, vn0,  # initial guess for primal and dual variables 
-        L, nu21, nun,  # spectral norm 
-        sig_21, sig_n, # regulariser strengths
-        weights_21, weights_n,
-        gamma_21, gamma_n,  # step sizes
-        lam_21, lam_n, lam_p,  # extrapolation
-        tol=1e-3, maxit=1000, positivity=True, report_freq=10):
+def hpd(fprime, prox, reg, x0, gamma, beta, sig_21, 
+        hess=None,
+        cgprecond=None,
+        cgtol=1e-3,
+        cgmaxit=35,
+        alpha0=0.25,
+        alpha_ff=0.2,
+        reweight_start=20,
+        reweight_freq=5,
+        tol=1e-3, 
+        maxit=1000,
+        report_freq=1):
     """
     Algorithm to solve problems of the form
 
-    argmin_x (b - x).T A (b - x) + lam_21 |x|_21 + lam_n |s|_1
+    argmin_x F(x) + lam_n |s|_1 + lam_21 |L x|_21
 
     where x is the image cube and s are the singular values along 
     the frequency dimension and there is an optional positivity 
     constraint. 
 
-    L        - Lipschitz constant of A
-    sig_21   - strength of l21 regulariser
-    sig_n    - strength of nuclear norm regulariser
-    gamma_21 - l21 step size
-    gamma_n  - nuclear step size
-    lam_21   - extrapolation parameter for l21 step
-    lam_n    - extrapolation parameter for nuclear step
-    lam_p    - extrapolation parameter for primal step
-    
-    # Note - Spectral norms for both decomposition operators are unity.
+    fprime      - function that produces F(x), grad F(x)
+    x           - primal variable
+    y           - dual variable
+    gamma       - initial step-size
+    beta        - Lipschitz constant of F
+    sig_21      - strength of l21 regulariser
+
     """
     nchan, nx, ny = x0.shape
     npix = nx*ny
 
-    # set up dual variables
-    v21 = v210
-    vn = vn0
+    # weights
+    weights_21 = np.ones(npix, dtype=np.float64)
 
-    # gradient function
-    grad_func = lambda x: -A(b-x)
+    # initialise
+    x = x0
 
-    # stepsize control
-    tau = 0.95/(L/2.0 + gamma_21 + gamma_n)
+    # initial fidelity and gradient
+    fid, gradn = fprime(x)
+    regx = reg(x)
+    # initial objective and fidelity funcs
+    fidelity = np.zeros(maxit+1)
+    objective = np.zeros(maxit+1)
+    regulariser = np.zeros(maxit+1)
+    fidelity[0] = fid
+    regulariser[0] = regx
+    objective[0] = fid + regx
 
     # start iterations
-    x = x0.copy()
     eps = 1.0
     k = 0
+    i = 0  # reweighting counter
     for k in range(maxit):
         xp = x.copy()
-
         # gradient
-        grad = grad_func(x)
+        gradp = gradn
 
-        # primal update
-        p = x - tau*(grad + vn.reshape(nchan, nx, ny) + v21.reshape(nchan, nx, ny))
-        if positivity:
-            p[p<0] = 0.0
-        
-        x = x + lam_p*(p - xp)
+        # get update 
+        if hess is not None:
+            delx = pcg(hess, -gradp, np.zeros(x.shape), M=cgprecond, tol=cgtol, maxit=cgmaxit, verbosity=0)
+        else:
+            delx = -gradp
+        p = xp + gamma * delx
+
+        x = prox(p, sig_21, weights_21)
+
+        fid, gradn = fprime(x)
+        regx = reg(x)
+        objective[k+1] = fid + regx
+        fidelity[k+1] = fid
+        regulariser[k+1] = regx
+        while objective[k+1] >= objective[k] and k < reweight_start and k%reweight_freq:
+            gamma *= 0.9
+            print("Step size too large, adjusting gamma = %f"%gamma)
+            
+            p = xp + gamma * delx
+
+            x = prox(p, sig_21, weights_21)
+
+            fid, gradn = fprime(x)
+            regx = reg(x)
+            objective[k+1] = fid + regx
+            fidelity[k+1] = fid
+            regulariser[k+1] = regx
 
         # convergence check
-        eps = norm(x-xp)/norm(x)
+        normx = norm(x)
+        if np.isnan(normx) or normx == 0.0:
+            normx = 1.0
+        
+        eps = norm(x-xp)/normx
         if eps < tol:
             break
 
-        if not k%10:
-            print("At iteration %i eps = %f and current stepsize is %f"%(k, eps, L))
+        if k >= reweight_start and not k%reweight_freq:
+            alpha = alpha0/(1+i)**alpha_ff
+            normx = norm(x.reshape(nchan, npix), axis=0)
+            weights_21 = 1.0/(normx + alpha)
+            i += 1
 
-        # Vector on which dual operations take place flattened into matrix
-        xtmp = (2*p - xp).reshape(nchan, npix)
+        if not k%report_freq:
+            print("At iteration %i eps = %f, norm21 = %f "%(k, eps, regx))
 
-        # Nuclear dual update
-        U, s, Vh = svd(vn + xtmp, full_matrices=False)
-        s = np.maximum(np.abs(s) - gamma_n * sig_n * weights_n, 0.0) * np.sign(s)
-        vn = vn + lam_n*(xtmp - U.dot(s[:, None] * Vh))
-
-        # l21 dual update
-        r21 = v21 + xtmp
-        l2norm = norm(r21, axis=0)
-        l2_soft = np.maximum(np.abs(l2norm) - gamma_21 * sig_21 * weights_21, 0.0) * np.sign(l2norm)
-        indices = np.nonzero(l2norm)
-        ratio = np.zeros(l2norm.shape, dtype=np.float64)
-        ratio[indices] = l2_soft[indices]/l2norm[indices]
-        v21 = v21 + lam_21*(xtmp - ratio[None, :] * r21)
 
     if k == maxit-1:
         print("HPD - Maximum iterations reached. Relative difference between updates = ", eps)
     else:
         print("HPD - Success, converged after %i iterations"%k)
 
-    return x, v21, vn       
+    return x, objective, fidelity, regulariser
 
 def power_method(A, imsize, tol=1e-5, maxit=250):
     b = np.random.randn(*imsize)
