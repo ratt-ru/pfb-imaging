146d523bad348cd856a2c23722c854aa953d134e
landmanbester <lbester@ska.ac.za>
2020-04-17 16:40:23 +0200

    Remove undefined real_type in scratch.py

--- scratch.py
+++ scratch.py
@@ -17,9 +17,9 @@ if __name__=="__main__":
 
     p = np.ones((nchan, nx, ny), dtype=np.float64)
     weights_21 = np.empty(nbasis, dtype=object)
-    weights_21[0] = np.ones(nx*ny, dtype=real_type)  # for dirac basis
+    weights_21[0] = np.ones(nx*ny, dtype=np.float64)  # for dirac basis
     for m in range(1, psi.nbasis):
-        weights_21[m] = np.ones(psi.ntot, dtype=real_type)  # all other wavelet bases
+        weights_21[m] = np.ones(psi.ntot, dtype=np.float64)  # all other wavelet bases
     
     sig_21 = 1e-5
 
