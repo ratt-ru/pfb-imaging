f061a1aef926a5dba7f8b61df7d22c36658ea1d6
landmanbester <lbester@ska.ac.za>
2020-09-10 14:38:35 +0200

    Rename pfb.py to pfbclean.py

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import pfb
 import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -5,13 +6,13 @@ with open("README.md", "r") as fh:
 setuptools.setup(
      name='pfb',  
      version='0.0.1',
-     scripts=['scripts/simple_spi_fitter.py',
-              'scripts/power_beam_maker.py',
-              'scripts/image_convolver.py',
-              'scripts/pfb.py',
-              'scripts/solve_x0.py',
-              'scripts/make_dirty.py',
-              'scripts/prep_data.py'] ,
+    #  scripts=['scripts/simple_spi_fitter.py',
+    #           'scripts/power_beam_maker.py',
+    #           'scripts/image_convolver.py',
+    #           'scripts/pfb.py',
+    #           'scripts/solve_x0.py',
+    #           'scripts/make_dirty.py',
+    #           'scripts/prep_data.py'] ,
      author="Landman Bester",
      author_email="lbester@ska.ac.za",
      description="Pre-conditioned forward-backward CLEAN algorithm",
