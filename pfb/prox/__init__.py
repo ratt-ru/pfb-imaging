30caf6f732cd6e318dcd2046e1cc24aed969816d
landmanbester <lbester@ska.ac.za>
2021-04-06 16:27:28 +0200

    Add __init__.py for prox module

---
+++ pfb/prox/__init__.py
@@ -0,0 +1,10 @@
+"""
+Pre-conditioned Forward Backward Clean algorithm
+
+author - Landman Bester
+email  - lbester@ska.ac.za
+date   - 31/03/2020
+"""
+
+from pfb.prox.prox_21 import prox_21
+from pfb.prox.prox_21m import prox_21m
