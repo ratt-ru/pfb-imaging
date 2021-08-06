d51a8ecebd24429c7e8f501ca34ecae482deecd2
landmanbester <lbester@ska.ac.za>
2021-08-06 09:38:09 +0200

    transcols worker

--- pfb/workers/main.py
+++ pfb/workers/main.py
@@ -12,4 +12,4 @@ from pfb.workers.grid import dirty, psf, predict, residual
 from pfb.workers.post import spifit, binterp
 from pfb.workers.deconv import nnls, clean, forward
 from pfb.workers.weighting import imweight
-from pfb.workers.misc import jones2col
+from pfb.workers.misc import jones2col, transcols
