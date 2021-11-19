hess normalisation wrong

pfb/operators/hessian.py
@@ -6,7 +6,7 @@ from daskms.optimisation import inlined_array
 from ducc0.wgridder import ms2dirty, dirty2ms
 from pfb.operators.psi import im2coef, coef2im
 
-def hessian_wgt_xds(x, xdss, hessopts, wsum, sigmainv, compute=True, use_beam=True):
+def hessian_xds(x, xdss, hessopts, wsum, sigmainv, compute=True, use_beam=True):
     '''
     Vis space Hessian reduction over dataset
     '''
@@ -26,7 +26,7 @@ def hessian_wgt_xds(x, xdss, hessopts, wsum, sigmainv, compute=True, use_beam=Tr
             # unnecessary beam application
             beam = da.ones_like(x)
 
-        convim = hessian_wgt(uvw, wgt, freq, x, beam, fbin_idx,
+        convim = hessian(uvw, wgt, freq, x, beam, fbin_idx,
                              fbin_counts, hessopts)
         convim = inlined_array(convim, uvw)
 
@@ -43,7 +43,7 @@ def hessian_wgt_xds(x, xdss, hessopts, wsum, sigmainv, compute=True, use_beam=Tr
     else:
         return convim
 
-def hessian_wgt_alpha_xds(alpha, xdss, waveopts, hessopts, wsum,
+def hessian_alpha_xds(alpha, xdss, waveopts, hessopts, wsum,
                           sigmainv, compute=True):
     '''
     Vis space Hessian reduction over dataset
@@ -75,8 +75,8 @@ def hessian_wgt_alpha_xds(alpha, xdss, waveopts, hessopts, wsum,
         fbin_counts = xds.FBIN_COUNTS.data
         beam = xds.BEAM.data
 
-        convim = hessian_wgt(uvw, wgt, freq, x, beam, fbin_idx,
-                             fbin_counts, hessopts)
+        convim = hessian(uvw, wgt, freq, x, beam, fbin_idx,
+                         fbin_counts, hessopts)
         convim = inlined_array(convim, uvw)
 
         convims.append(convim)
@@ -84,7 +84,6 @@ def hessian_wgt_alpha_xds(alpha, xdss, waveopts, hessopts, wsum,
     # LB - it's not this simple when there are multiple spw's mapping to
     # different imaging bands
     convim = da.stack(convims).sum(axis=0)/wsum
-
 
     alpha_rec = im2coef(convim, pmask, bases, ntot, nmax, nlevels)
     alpha_rec = inlined_array(alpha_rec, [pmask, bases, ntot])
@@ -97,12 +96,12 @@ def hessian_wgt_alpha_xds(alpha, xdss, waveopts, hessopts, wsum,
     else:
         return alpha_rec
 
-def _hessian_wgt_impl(uvw, weight, freq, x, beam, fbin_idx, fbin_counts,
-                      cell=None,
-                      wstack=None,
-                      epsilon=None,
-                      double_accum=None,
-                      nthreads=None):
+def _hessian_impl(uvw, weight, freq, x, beam, fbin_idx, fbin_counts,
+                  cell=None,
+                  wstack=None,
+                  epsilon=None,
+                  double_accum=None,
+                  nthreads=None):
     nband, nx, ny = beam.shape
     convim = np.zeros((nband, nx, ny), dtype=x.dtype)  # no row chunking
     fbin_idx2 = fbin_idx - fbin_idx.min()  # adjust for freq chunking
@@ -138,14 +137,14 @@ def _hessian_wgt_impl(uvw, weight, freq, x, beam, fbin_idx, fbin_counts,
     return convim
 
 
-def _hessian_wgt(uvw, weight, freq, x, beam, fbin_idx, fbin_counts, hessopts):
-    return _hessian_wgt_impl(uvw[0][0], weight[0], freq, x, beam,
+def _hessian(uvw, weight, freq, x, beam, fbin_idx, fbin_counts, hessopts):
+    return _hessian_impl(uvw[0][0], weight[0], freq, x, beam,
                              fbin_idx, fbin_counts, **hessopts)
 
-def hessian_wgt(uvw, weight, freq, x, beam, fbin_idx, fbin_counts, hessopts):
+def hessian(uvw, weight, freq, x, beam, fbin_idx, fbin_counts, hessopts):
 
-    return da.blockwise(_hessian_wgt, ('chan', 'nx', 'ny')
-,                       uvw, ("row", 'three'),
+    return da.blockwise(_hessian, ('chan', 'nx', 'ny'),
+                        uvw, ("row", 'three'),
                         weight, ('row', 'chan'),
                         freq, ('chan',),
                         x, ('chan', 'nx', 'ny'),

pfb/operators/psf.py
@@ -1,9 +1,55 @@
 import numpy as np
 import dask.array as da
+from daskms.optimisation import inlined_array
 from ducc0.fft import r2c, c2r, c2c, good_size
 from pfb.operators.psi import im2coef, coef2im
 iFs = np.fft.ifftshift
 Fs = np.fft.fftshift
+
+
+def psf_convolve_alpha_xds(alpha, xdss, psfopts, waveopts, wsum, sigmainv, compute=True):
+    '''
+    Image space Hessian reduction over dataset
+    '''
+    pmask = waveopts['pmask']
+    bases = waveopts['bases']
+    padding = waveopts['padding']
+    iy = waveopts['iy']
+    sy = waveopts['sy']
+    nx = waveopts['nx']
+    ny = waveopts['ny']
+    ntot = waveopts['ntot']
+    nmax = waveopts['nmax']
+    nlevels = waveopts['nlevels']
+
+    if not isinstance(alpha, da.Array):
+        alpha = da.from_array(alpha, chunks=(1, -1, -1), name=False)
+
+    # coeff to image
+    x = coef2im(alpha, pmask, bases, padding, iy, sy, nx, ny)
+    x = inlined_array(x, [pmask, alpha, bases, padding])
+
+    convims = []
+    for xds in xdss:
+        psfhat = xds.PSFHAT.data
+        beam = xds.BEAM.data
+        convim = psf_convolve(x, psfhat, beam, psfopts)
+        convims.append(convim)
+
+    # LB - it's not this simple when there are multiple spw's to consider
+    convim = da.stack(convims).sum(axis=0)/wsum
+
+    alpha_rec = im2coef(convim, pmask, bases, ntot, nmax, nlevels)
+    alpha_rec = inlined_array(alpha_rec, [pmask, bases, ntot])
+
+    if sigmainv:
+        alpha_rec += alpha * sigmainv**2
+
+    if compute:
+        return alpha_rec.compute()
+    else:
+        return alpha_rec
+
 
 def psf_convolve_xds(x, xdss, psfopts, wsum, sigmainv, compute=True):
     '''
@@ -11,7 +57,7 @@ def psf_convolve_xds(x, xdss, psfopts, wsum, sigmainv, compute=True):
     '''
     convims = []
     for xds in xdss:
-        psf = xds.PSF.data
+        psfhat = xds.PSFHAT.data
         beam = xds.BEAM.data
 
         convim = psf_convolve(x, psfhat, beam, psfopts)
@@ -51,7 +97,7 @@ def _psf_convolve_impl(x, psfhat, beam,
     return convim
 
 def _psf_convolve(x, psfhat, beam, psfopts):
-    return _psf_convolve_impl(x, psfhat, **psfopts)
+    return _psf_convolve_impl(x, psfhat, beam, **psfopts)
 
 def psf_convolve(x, psfhat, beam, psfopts):
     if not isinstance(x, da.Array):

pfb/opt/pcg.py
@@ -74,44 +74,44 @@ def pcg(A,
             print("Success, converged after %i iters" % k, file=log)
     return x
 
-from pfb.operators.hessian import _hessian_psf_impl as hessian_psf
-def _pcg_psf_impl(psfhat,
-                  b,
-                  x0,
-                  beam,
-                  hessopts,
-                  waveopts,
-                  tol=1e-5,
-                  maxit=500,
-                  minit=100,
-                  verbosity=1,
-                  report_freq=10,
-                  backtrack=True):
-    '''
-    A specialised distributed version of pcg when the operator implements
-    convolution with the psf (+ L2 regularisation by sigma**2)
-    '''
-    nband, nbasis, nmax = b.shape
-    model = np.zeros((nband, nbasis, nmax), dtype=b.dtype)
-    sigmainvsq = hessopts['sigmainv']**2
-    # PCG preconditioner
-    if sigmainvsq > 0:
-        def M(x): return x / sigmainvsq
-    else:
-        M = None
+# from pfb.operators.hessian import _hessian_psf_impl as hessian_psf
+# def _pcg_psf_impl(psfhat,
+#                   b,
+#                   x0,
+#                   beam,
+#                   hessopts,
+#                   waveopts,
+#                   tol=1e-5,
+#                   maxit=500,
+#                   minit=100,
+#                   verbosity=1,
+#                   report_freq=10,
+#                   backtrack=True):
+#     '''
+#     A specialised distributed version of pcg when the operator implements
+#     convolution with the psf (+ L2 regularisation by sigma**2)
+#     '''
+#     nband, nbasis, nmax = b.shape
+#     model = np.zeros((nband, nbasis, nmax), dtype=b.dtype)
+#     sigmainvsq = hessopts['sigmainv']**2
+#     # PCG preconditioner
+#     if sigmainvsq > 0:
+#         def M(x): return x / sigmainvsq
+#     else:
+#         M = None
 
-    for k in range(nband):
-        A = partial(hessian_psf,
-                    psfhat=psfhat[k],
-                    beam=beam[k],
-                    **hessopts,
-                    **waveopts)
-        model[k] = pcg(A, b[k], x0[k],
-                       M=M, tol=tol, maxit=maxit, minit=minit,
-                       verbosity=verbosity, report_freq=report_freq,
-                       backtrack=backtrack)
+#     for k in range(nband):
+#         A = partial(hessian_psf,
+#                     psfhat=psfhat[k],
+#                     beam=beam[k],
+#                     **hessopts,
+#                     **waveopts)
+#         model[k] = pcg(A, b[k], x0[k],
+#                        M=M, tol=tol, maxit=maxit, minit=minit,
+#                        verbosity=verbosity, report_freq=report_freq,
+#                        backtrack=backtrack)
 
-    return model
+#     return model
 
 # def _pcg_psf(psfhat,
 #              b,

pfb/workers/deconv/forward.py
@@ -204,17 +204,22 @@ def _forward(**kw):
     waveopts['ny'] = ny
     waveopts['padding'] = da.from_array(np.array(padding, dtype=object), chunks=-1)
 
+    hessopts = {}
+    hessopts['cell'] = xds[0].cell_rad
+    hessopts['wstack'] = args.wstack
+    hessopts['epsilon'] = args.epsilon
+    hessopts['double_accum'] = args.double_accum
+    hessopts['nthreads'] = args.nvthreads
+    wsum = wsum.compute()
+
     if not args.no_use_psf:
         print("Initialising psf", file=log)
+        from pfb.operators.psf import psf_convolve_alpha_xds
         from ducc0.fft import r2c
         normfact = 1.0
 
-        psf = xds.PSF.data
+        psf = xds[0].PSF.data
         _, nx_psf, ny_psf = psf.shape
-
-        psf /= wsum
-        psf_mfs = da.sum(psf, axis=0).compute()
-        assert (psf_mfs.max() - 1.0) < args.epsilon
 
         iFs = np.fft.ifftshift
 
@@ -226,52 +231,35 @@ def _forward(**kw):
         unpad_x = slice(npad_xl, -npad_xr)
         unpad_y = slice(npad_yl, -npad_yr)
         lastsize = ny + np.sum(padding_psf[-1])
-        psf_pad = iFs(psf.compute(), axes=(1, 2))
-        psfhat = r2c(psf_pad, axes=(1, 2), forward=True,
-                     nthreads=args.nthreads, inorm=0)
 
-        psfhat = da.from_array(psfhat, chunks=(1, -1, -1), name=False)
+        # add psfhat to Dataset
+        for i, ds in enumerate(xds):
+            psf_pad = iFs(ds.PSF.data.compute(), axes=(1, 2))
+            psfhat = r2c(psf_pad, axes=(1, 2), forward=True,
+                         nthreads=args.nthreads, inorm=0)
 
-        hessopts = {}
-        hessopts['padding_psf'] = padding_psf[1:]
-        hessopts['unpad_x'] = unpad_x
-        hessopts['unpad_y'] = unpad_y
-        hessopts['lastsize'] = lastsize
-        hessopts['nthreads'] = args.nvthreads
-        hessopts['sigmainv'] = args.sigmainv
+            psfhat = da.from_array(psfhat, chunks=(1, -1, -1), name=False)
+            ds = ds.assign({'PSFHAT':(('nband', 'x_psf', 'y_psfo2'), psfhat)})
+            xds[i] = ds
 
-        print("Solving for update using image space approximation", file=log)
-        def convolver(x):
-            model = da.from_array(x,
-                                  chunks=(1, nx, ny),
-                                  name=False)
+        psfopts = {}
+        psfopts['padding'] = padding_psf[1:]
+        psfopts['unpad_x'] = unpad_x
+        psfopts['unpad_y'] = unpad_y
+        psfopts['lastsize'] = lastsize
+        psfopts['nthreads'] = args.nvthreads
 
-
-            convolvedim = hessian(model,
-                                  psfhat,
-                                  padding,
-                                  args.nvthreads,
-                                  unpad_x,
-                                  unpad_y,
-                                  lastsize)
-            return convolvedim
+        hess = partial(psf_convolve_alpha_xds, xdss=xds, waveopts=waveopts,
+                       psfopts=psfopts, sigmainv=args.sigmainv, wsum=wsum, compute=True)
 
     else:
         print("Solving for update using vis space approximation", file=log)
-        from pfb.operators.hessian import hessian_wgt_alpha_xds
+        from pfb.operators.hessian import hessian_alpha_xds
 
-        hessopts = {}
-        hessopts['cell'] = xds[0].cell_rad
-        hessopts['wstack'] = args.wstack
-        hessopts['epsilon'] = args.epsilon
-        hessopts['double_accum'] = args.double_accum
-        hessopts['nthreads'] = args.nvthreads
-        wsum = wsum.compute()
+        hess = partial(hessian_alpha_xds, xdss=xds, waveopts=waveopts,
+                       hessopts=hessopts, sigmainv=args.sigmainv, wsum=wsum, compute=True)
 
-        hess = partial(hessian_wgt_alpha_xds, xdss=xds, waveopts=waveopts,
-                       hessopts=hessopts, sigmainv=args.sigmainv, wsum=wsum, compute=False)
-
-    # import pdb; pdb.set_trace()
+    # # import pdb; pdb.set_trace()
     # x = np.random.randn(nband, nbasis, nmax).astype(np.float32)
     # res = hess(x)
     # dask.visualize(res, color="order", cmap="autumn",
@@ -280,6 +268,9 @@ def _forward(**kw):
     #                optimize_graph=False)
     # dask.visualize(res, filename=args.output_filename +
     #                '_hess_I_graph.pdf', optimize_graph=False)
+
+    # import pdb; pdb.set_trace()
+    # print(res)
 
     # quit()
 
@@ -309,11 +300,13 @@ def _forward(**kw):
                     pmask, waveopts['bases'], waveopts['padding'],
                     iys, sys, nx, ny)
 
-    from pfb.operators.hessian import hessian_wgt_xds
-    residual -= hessian_wgt_xds(model, xds, hessopts, wsum, 0.0, compute=False)
+    # from pfb.operators.hessian import hessian_xds
+    # residual -= hessian_xds(model, xds, hessopts, wsum, 0.0, compute=True)
+    from pfb.operators.psf import psf_convolve_xds
+    residual -= psf_convolve_xds(model, xds, psfopts, wsum, 0.0, compute=True)
 
-    residual = dask.compute(residual,
-                            optimize_graph=False)[0]
+    # residual = dask.compute(residual,
+    #                         optimize_graph=False)[0]
 
     # construct a header from xds attrs
     ra = xds[0].ra