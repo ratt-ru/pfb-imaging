f649afa19c83277b8847f735ad520380ad1d0aa5
landmanbester <lbester@ska.ac.za>
2022-02-14 14:22:48 +0200

    setup.py weirdness

--- setup.py
+++ setup.py
@@ -33,8 +33,8 @@ requirements = [
                 "@master",
 
                 "scabha"
-                "@git+https://github.com/ratt-ru/scabha.git"
-                "@master"
+                "@git+https://github.com/caracal-pipeline/scabha2"
+                #"@master#egg=scabha-0.4.0",
             ]
 
 
