c0f66f9996fac29cdbb024ba49734c6bab69bc84
landmanbester <lbester@ska.ac.za>
2022-02-16 14:37:47 +0200

    stimela not finding stimela_cabs.yaml

--- pfb/utils/fits.py
+++ pfb/utils/fits.py
@@ -29,7 +29,7 @@ def set_wcs(cell_x, cell_y, nx, ny, radec, freq, unit='Jy/beam'):
     """
     cell_x/y - cell sizes in degrees
     nx/y - number of x and y pixels
-    radec - right ascention and declination in degrees
+    radec - right ascention and declination in radians
     freq - frequencies in Hz
     """
 
