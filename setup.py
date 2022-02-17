91d82a449d6fc49484636889cdff55d7cbbf790c
landmanbester <lbester@ska.ac.za>
2022-02-17 13:26:54 +0200

    Add scabha dependency to setup.py

--- setup.py
+++ setup.py
@@ -31,6 +31,8 @@ requirements = [
                 "packratt"
                 "@git+https://github.com/ratt-ru/packratt.git"
                 "@master",
+
+                "scabha @ git+https://github.com/caracal-pipeline/scabha2",
             ]
 
 
