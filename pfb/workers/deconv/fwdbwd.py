Fix wsum normalisation for fits cubes in fwdbwd

pfb/workers/deconv/fwdbwd.py
@@ -299,8 +299,7 @@ def _fwdbwd(**kw):
             wsums[b] += ds.WSUM.values[0]
             residual[b] += ds.RESIDUAL.values
         wsum = np.sum(wsums)
-        residual /= wsum
-        residual_mfs = np.sum(residual, axis=0)
+        residual_mfs = np.sum(residual, axis=0)/wsum
         save_fits(f'{basename}_residual_mfs.fits', residual_mfs, hdr_mfs)
 
         if opts.fits_cubes:

pfb/workers/misc/forward.py
@@ -251,9 +251,8 @@ def _forward(**kw):
                 wsums[b] += ds.WSUM.values
                 residual[b] += ds.FORWARD_RESIDUAL.values.astype(np.float32)
             wsum = np.sum(wsums)
-            residual /= wsum
 
-            residual_mfs = np.sum(residual, axis=0)
+            residual_mfs = np.sum(residual, axis=0)/wsum
             save_fits(f'{basename}{opts.postfix}_forward_residual_mfs.fits',
                       residual_mfs, hdr_mfs)
 