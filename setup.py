21db341d61c278f829272c6a09bc8a9cae745018
landmanbester <lbester@ska.ac.za>
2022-07-14 13:18:48 +0200

    include_package_data=True in setup.py + add fwdbwd config to MANIFEST.in

--- setup.py
+++ setup.py
@@ -50,6 +50,8 @@ setup(
      long_description_content_type="text/markdown",
      url="https://github.com/ratt-ru/pfb-clean",
      packages=find_packages(),
+     include_package_data=True,
+     zip_safe=False,
      python_requires='>=3.7',
      install_requires=requirements,
      classifiers=[
