a57a21270d772c699ed147ecc10d0bb054cb05d8
landmanbester <lbester@ska.ac.za>
2023-02-02 16:24:13 +0200

    Revert "start pd from update if model is zero"
    This reverts commit b664c6ef145c7cd8aed85844ccf2e7c5ff10c956.

--- pfb/workers/spotless.py
+++ pfb/workers/spotless.py
@@ -9,6 +9,15 @@ import pyscilog
 pyscilog.init('pfb')
 log = pyscilog.get_logger('SPOTLESS')
 
+from numba import njit
+@njit
+def showtys(iy):
+    print(len(iy), iy)
+    # for n in range(1, len(tys)):
+    #     for k, v in tys[n].items():
+    #         print(k, v)
+    return
+
 from scabha.schema_utils import clickify_parameters
 from pfb.parser.schemas import schema
 
@@ -312,7 +321,7 @@ def _spotless(**kw):
         data = model + opts.gamma*update
         model, dual = primal_dual(hess_psf,
                                   data,
-                                  model if np.any(model) else update,
+                                  model,
                                   dual,
                                   opts.rmsfactor*rms_comps,
                                   psi,
