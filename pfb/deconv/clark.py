fix psf normalisation in clark + full output name

pfb/deconv/clark.py
@@ -79,6 +79,7 @@ def subminor(A, psf, Ip, Iq, model, wsums, gamma=0.05, th=0.0, maxit=10000):
 def clark(ID,
           PSF,
           PSFHAT,
+          wsums,
           threshold=0,
           gamma=0.05,
           pf=0.05,
@@ -92,13 +93,11 @@ def clark(ID,
           nthreads=1):
     nband, nx, ny = ID.shape
     _, nx_psf, ny_psf = PSF.shape
-    wsums = np.amax(PSF, axis=(1,2))
     wsum = wsums.sum()
-    # normalise so the PSF always sums to 1
     ID /= wsum
     PSF /= wsum
     PSFHAT /= wsum
-    wsums = np.amax(PSF, axis=(1,2))
+    wsums = wsums/wsum
     model = np.zeros((nband, nx, ny), dtype=ID.dtype)
     model = make_noncritical(model)
     IR = ID.copy()