0ec4d6fb1a13bea04d0b8a804c6077456b43da10
landmanbester <lbester@ska.ac.za>
2023-02-27 22:37:39 +0200

    bsmooth using slow kanterp

--- pfb/workers/misc/bsmooth.py
+++ pfb/workers/misc/bsmooth.py
@@ -130,15 +130,15 @@ def _bsmooth(**kw):
                     idx = w>0
                     amplin = np.interp(freq, freq[idx], y[idx])
                     I = slice(128, -128)
-                    ms, Ps = kanterp(nu[I], amplin[I], w[I], niter=10, nu0=2,
-                                      sigmaf0=np.sqrt(nchan), sigman0=1)
+                    ms, Ps = kanterp(nu[I], amplin[I], w[I], niter=10, nu0=2)
+                                    #  ,sigmaf0=np.sqrt(nchan), sigman0=1)
                     amp[0, I, p, 0, c] = ms[0, :]
                     if p == ref_ant:
                         continue
                     y = phase[0, :, p, 0, c]
                     phaselin = np.interp(freq, freq[idx], y[idx])
                     ms, Ps = kanterp(nu[I], phaselin[I], w[I]/amp[0, I, p, 0, c],
-                                      niter=10, nu0=2, sigmaf0=np.sqrt(nchan), sigman0=1)
+                                      niter=10, nu0=2) #, sigmaf0=np.sqrt(nchan), sigman0=1)
                     phase[0, I, p, 0, c] = ms[0, :]
 
 
@@ -172,15 +172,15 @@ def _bsmooth(**kw):
                 amp = bamp[0, :, p, 0, c]
                 amplin = np.interp(freq, freq[idx], amp[idx])
                 I = slice(128, -128)
-                ms, Ps = kanterp(nu[I], amplin[I], w[I], niter=10, nu0=2,
-                                  sigmaf0=np.sqrt(nchan), sigman0=1)
+                ms, Ps = kanterp(nu[I], amplin[I], w[I], niter=10, nu0=2)
+                                #  ,sigmaf0=np.sqrt(nchan), sigman0=1)
                 samp[0, I, p, 0, c] = ms[0, :]
                 if p == ref_ant:
                     continue
                 phase = bphase[0, :, p, 0, c]
                 phaselin = np.interp(freq, freq[idx], phase[idx])
-                ms, Ps = kanterp(nu[I], phaselin[I], w[I]/samp[0, I, p, 0, c],
-                                  niter=10, nu0=2, sigmaf0=np.sqrt(nchan), sigman0=1)
+                ms, Ps = kanterp(nu[I], phaselin[I], w[I]/samp[0, I, p, 0, c]) #,
+                                #   niter=10, nu0=2, sigmaf0=np.sqrt(nchan), sigman0=1)
                 sphase[0, I, p, 0, c] = ms[0, :]
 
 
