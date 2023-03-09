eb39df5790adddd9525efd366ec01943676b42f9
landmanbester <lbester@ska.ac.za>
2023-03-09 11:21:11 +0200

    new naming convention for spotless (non-distributed version)

--- pfb/workers/spotless.py
+++ pfb/workers/spotless.py
@@ -157,7 +157,7 @@ def _spotless(**kw):
 
     basename = f'{opts.output_filename}_{opts.product.upper()}'
 
-    dds_name = f'{basename}{opts.postfix}.dds.zarr'
+    dds_name = f'{basename}_{opts.postfix}.dds.zarr'
     dds = xds_from_zarr(dds_name, chunks={'row':-1,
                                           'chan':-1,
                                           'x':-1,
