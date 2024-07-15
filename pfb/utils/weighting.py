fix slow im weight computation

pfb/utils/weighting.py
@@ -18,16 +18,25 @@ def compute_counts(dsl,
                    cell_size_x, cell_size_y,
                    k=6,
                    nthreads=1):
+    '''
+    Sum the weights on the grid over all datasets
+    '''
 
-    if isinstance(dsl[0], str):
-        dsl = xds_from_list(dsl)
+    if isinstance(dsl, str):
+        dsl = [dsl]
+
+    dsl = xds_from_list(dsl, nthreads=nthreads,
+                        drop_vars=('VIS','BEAM'))
 
     nds = len(dsl)
-    ngrid = np.maximum(nthreads//nds, 1)
+    maxw = np.minimum(nthreads, nds)
+    if nthreads > nds:  # this is not perfect
+        ngrid = np.maximum(nthreads//nds, 1)
+    else:
+        ngrid = 1
 
-    counts = []
-    # TODO - parallel over ds
-    with cf.ThreadPoolExecutor(max_workers=nds) as executor:
+    counts = np.zeros((nx, ny), dtype=dsl[0].WEIGHT.dtype)
+    with cf.ThreadPoolExecutor(max_workers=maxw) as executor:
         futures = []
         for ds in dsl:
             fut = executor.submit(_compute_counts,
@@ -44,10 +53,10 @@ def compute_counts(dsl,
             futures.append(fut)
 
         for fut in cf.as_completed(futures):
-            # sum over number of grids
-            counts.append(fut.result().sum(axis=0))
+            # sum over number of grids and number of datasets
+            counts += fut.result().sum(axis=0)
 
-    return np.array(counts)
+    return counts
 
 
 
@@ -126,7 +135,7 @@ def _es_kernel(x, beta, k):
     return np.exp(beta*k*(np.sqrt((1-x)*(1+x)) - 1))
 
 
-@njit(nogil=True, cache=True)
+@njit(nogil=True, cache=True, parallel=True)
 def counts_to_weights(counts, uvw, freq, nx, ny,
                        cell_size_x, cell_size_y, robust):
     # ufreq

pfb/workers/grid.py
@@ -209,6 +209,7 @@ def _grid(xdsi=None, **kw):
         from pfb.utils.dist import fake_client
         client = fake_client()
         names = [0]
+        as_completed = lambda x: x
 
     basename = f'{opts.output_filename}'
 
@@ -373,14 +374,17 @@ def _grid(xdsi=None, **kw):
                                 nx, ny,
                                 cell_rad, cell_rad,
                                 nthreads=opts.nthreads,
+                                tbid=tbid,
                                 workers=wname)
 
             futures.append(fut)
-
-        counts = client.gather(futures)
-        counts = np.stack(counts, axis=0)
-        # band assumed to be axis 1
-        counts = np.transpose(counts, axes=(1,0,2,3))
+        counts = np.zeros((ntime, nband, nx, ny))
+        for fut in as_completed(futures):
+            count, tbid = fut.result()
+            bandid = tbid[-4:]
+            timeid = tbid[4:8]
+            counts[int(timeid), int(bandid)] = count
+            print(f'Done with {tbid}', file=log)
         if opts.mf_weighting:
             counts = np.sum(counts, axis=(1), keepdims=True)
         level = opts.filter_counts_level
@@ -399,7 +403,6 @@ def _grid(xdsi=None, **kw):
     if opts.psf:
         print(f"PSF size = (ntime={ntime}, nband={nband}, "
               f"nx={nx_psf}, ny={ny_psf})", file=log)
-
 
     # check if model exists
     if opts.transfer_model_from:
@@ -542,10 +545,6 @@ def _grid(xdsi=None, **kw):
 
     residual_mfs = np.zeros((nx, ny))
     wsum = 0.0
-    if opts.nworkers > 1:
-        from distributed import as_completed
-    else:
-        as_completed = lambda x: x
     nds = len(futures)
     n_launched = 1
     for fut in as_completed(futures):