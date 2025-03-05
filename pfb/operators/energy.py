88edaba245dbe32ed1ad562d61d56a94fe6fe1dc
landmanbester <lbester@sarao.ac.za>
2025-03-05 15:57:15 +0200

    polfusion

--- pfb/operators/energy.py
+++ pfb/operators/energy.py
@@ -7,9 +7,11 @@ from jax._src.scipy.sparse.linalg import _vdot_tree
 import nifty8.re as jft
 from functools import partial
 from ducc0.wgridder import dirty2vis, vis2dirty
+from ducc0.fft import c2c, r2c, c2r
 from numpy.fft import fftshift, ifftshift
 Fs = fftshift
 iFs = ifftshift
+import matplotlib.pyplot as plt
 
 
 @jax.jit
@@ -23,11 +25,7 @@ def pol_energy_approx(ID, psfhat, x):
     Id = jax.lax.stop_gradient(ID)
     nx_psf, ny_psf, _ = psfh.shape
     nx, ny, _ = Id.shape
-    # # Brightness
-    # a = x[:, :, 0]
-    # b = x[:, :, 1]
-    # c = x[:, :, 2]
-    # d = x[:, :, 3]
+    # Convert to brightness
     a = x[0:n*n].reshape(n, n)
     b = x[n*n:2*n*n].reshape(n, n)
     c = x[2*n*n:3*n*n].reshape(n, n)
@@ -118,15 +116,18 @@ if __name__=="__main__":
     freq = np.array([1e9])
 
     lightspeed = 299792458.0
-    cell_N = 1.0 / (2 * 2 * freq[0] / lightspeed)
+    uv_max = 2.0  # in (-1, 1)
+    cell_N = 1.0 / (2 * uv_max * freq[0] / lightspeed)
+    epsilon = 1e-6  # gridding precision
 
+    # convert Stokes to vis
     vis_I = dirty2vis(
         uvw=uvw,
         freq=freq,
         dirty=I,
         pixsize_x=cell_N,
         pixsize_y=cell_N,
-        epsilon=1e-6,
+        epsilon=epsilon,
     )
 
     vis_Q = dirty2vis(
@@ -135,7 +136,7 @@ if __name__=="__main__":
         dirty=Q,
         pixsize_x=cell_N,
         pixsize_y=cell_N,
-        epsilon=1e-6,
+        epsilon=epsilon,
     )
 
     vis_U = dirty2vis(
@@ -144,7 +145,7 @@ if __name__=="__main__":
         dirty=U,
         pixsize_x=cell_N,
         pixsize_y=cell_N,
-        epsilon=1e-6,
+        epsilon=epsilon,
     )
 
     vis_V = dirty2vis(
@@ -153,9 +154,10 @@ if __name__=="__main__":
         dirty=V,
         pixsize_x=cell_N,
         pixsize_y=cell_N,
-        epsilon=1e-6,
+        epsilon=epsilon,
     )
 
+    # convert Stokes vis to corr
     vis = np.zeros((m, 1, 4), dtype=np.complex128)
     vis[:, :, 0] = vis_I + vis_Q
     vis[:, :, 1] = vis_U + 1j*vis_V
@@ -167,36 +169,37 @@ if __name__=="__main__":
     dirty0 = vis2dirty(uvw=uvw, freq=freq, vis=vis[:, :, 0],
                        npix_x=n, npix_y=n,
                        pixsize_x=cell_N, pixsize_y=cell_N,
-                       epsilon=1e-6, do_wgridding=False,
+                       epsilon=epsilon, do_wgridding=False,
                        nthreads=8).astype("f8")
     dirty1 = vis2dirty(uvw=uvw, freq=freq, vis=vis[:, :, 1],
                        npix_x=n, npix_y=n,
                        pixsize_x=cell_N, pixsize_y=cell_N,
-                       epsilon=1e-6, do_wgridding=False,
+                       epsilon=epsilon, do_wgridding=False,
                        nthreads=8).astype("f8") \
                 +1j * vis2dirty(uvw=uvw, freq=freq, vis=-1j*vis[:, :, 1],
                                 npix_x=n, npix_y=n,
                                 pixsize_x=cell_N, pixsize_y=cell_N,
-                                epsilon=1e-6, do_wgridding=False,
+                                epsilon=epsilon, do_wgridding=False,
                                 nthreads=8).astype("f8")
     dirty2 = vis2dirty(uvw=uvw, freq=freq, vis=vis[:, :, 2],
                        npix_x=n, npix_y=n,
                        pixsize_x=cell_N, pixsize_y=cell_N,
-                       epsilon=1e-6, do_wgridding=False,
+                       epsilon=epsilon, do_wgridding=False,
                        nthreads=8).astype("f8") \
                 +1j * vis2dirty(uvw=uvw, freq=freq, vis=-1j*vis[:, :, 2],
                                 npix_x=n, npix_y=n,
                                 pixsize_x=cell_N, pixsize_y=cell_N,
-                                epsilon=1e-6, do_wgridding=False,
+                                epsilon=epsilon, do_wgridding=False,
                                 nthreads=8).astype("f8")
     dirty3 = vis2dirty(uvw=uvw, freq=freq, vis=vis[:, :, 3],
                        npix_x=n, npix_y=n,
                        pixsize_x=cell_N, pixsize_y=cell_N,
-                       epsilon=1e-6, do_wgridding=False,
+                       epsilon=epsilon, do_wgridding=False,
                        nthreads=8).astype("f8")
     
     ID = np.stack((dirty0, dirty1, dirty2, dirty3), axis=-1)
 
+    # in the absence of gains the PSF for each correlation should be the same
     psf = vis2dirty(uvw=uvw, freq=freq, vis=2*np.ones((m, 1), dtype=np.complex128),
                     npix_x=2*n, npix_y=2*n,
                     pixsize_x=cell_N, pixsize_y=cell_N,
@@ -208,44 +211,107 @@ if __name__=="__main__":
     dirtyI, dirtyQ, dirtyU, dirtyV = corr_to_stokes(ID, wsum=wsum)
 
 
-    import matplotlib.pyplot as plt
-    # plt.figure('1')
-    # plt.imshow(dirtyI)
-    # plt.colorbar()
+    plt.figure('1')
+    plt.imshow(dirtyI)
+    plt.colorbar()
 
-    # plt.figure('2')
-    # plt.imshow(dirtyQ)
-    # plt.colorbar()
+    plt.figure('2')
+    plt.imshow(dirtyQ)
+    plt.colorbar()
 
-    # plt.figure('3')
-    # plt.imshow(dirtyU)
-    # plt.colorbar()
+    plt.figure('3')
+    plt.imshow(dirtyU)
+    plt.colorbar()
 
-    # plt.figure('4')
-    # plt.imshow(dirtyV)
-    # plt.colorbar()
+    plt.figure('4')
+    plt.imshow(dirtyV)
+    plt.colorbar()
 
-
-    # plt.show()
-
-    # psfhat = np.fft.fft2(iFs(psf), axes=(0, 1), norm='backward')
-    # psfhat = np.tile(psfhat[:, :, None], (1, 1, 4))
+    plt.show()
 
 
-    # # test convolve
-    # S = np.stack([I, Q, U, V], axis=-1)
-    # B = np.stack(stokes_to_corr(S), axis=-1)
-    # # Convolution
-    # Bhat = jnp.fft.fft2(B,
-    #                     s=(2*n, 2*n),
-    #                     axes=(0, 1),
-    #                     norm='backward')
-    # Bconv = jnp.fft.ifft2(Bhat * psfhat,
-    #                       s=(2*n, 2*n),
-    #                       axes=(0, 1),
-    #                       norm='backward')[0:n, 0:n, :]/2
+    # we do not do the rfft because the corrs are complex
+    psfhat = np.fft.fft2(iFs(psf), axes=(0, 1), norm='backward')
+    # psfhat = c2c()
+    psfhat = np.tile(psfhat[:, :, None], (1, 1, 4))
+
+
+    # test convolve
+    S = np.stack([I, Q, U, V], axis=-1)
+    B = np.stack(stokes_to_corr(S), axis=-1)
+    # Convolution
+    Bhat = jnp.fft.fft2(B,
+                        s=(2*n, 2*n),
+                        axes=(0, 1),
+                        norm='backward')
+    Bconv = jnp.fft.ifft2(Bhat * psfhat,
+                          s=(2*n, 2*n),
+                          axes=(0, 1),
+                          norm='backward')[0:n, 0:n, :]/2
     
+    # convert back to stokes
+    dirtyI2, dirtyQ2, dirtyU2, dirtyV2 = corr_to_stokes(Bconv, wsum=wsum)
 
+    # compare Stokes dirty images
+    resI = np.abs(dirtyI - dirtyI2)
+    resQ = np.abs(dirtyQ - dirtyQ2)
+    resU = np.abs(dirtyU - dirtyU2)
+    resV = np.abs(dirtyV - dirtyV2)
+
+    assert np.allclose(resI, 0, atol=epsilon)
+    assert np.allclose(resQ, 0, atol=epsilon)
+    assert np.allclose(resU, 0, atol=epsilon)
+    assert np.allclose(resV, 0, atol=epsilon)
+
+    # compare corr dirty images
+    res0 = ID[:, :, 0] - Bconv[:, :, 0]
+    res1 = ID[:, :, 1] - Bconv[:, :, 1]
+    res2 = ID[:, :, 2] - Bconv[:, :, 2]
+    res3 = ID[:, :, 3] - Bconv[:, :, 3]
+
+
+    plt.figure('Dirty 0 real')
+    plt.imshow(res0.real/wsum)
+    plt.colorbar()
+
+    plt.figure('Dirty 0 imag')
+    plt.imshow(res0.imag/wsum)
+    plt.colorbar()
+
+    plt.figure('Dirty 1 real')
+    plt.imshow(res1.real/wsum)
+    plt.colorbar()
+
+    plt.figure('Dirty 1 imag')
+    plt.imshow(res1.imag/wsum)
+    plt.colorbar()
+
+    plt.figure('Dirty 2 real')
+    plt.imshow(res2.real/wsum)
+    plt.colorbar()
+
+    plt.figure('Dirty 2 imag')
+    plt.imshow(res2.imag/wsum)
+    plt.colorbar()
+
+    plt.figure('Dirty 3 real')
+    plt.imshow(res3.real/wsum)
+    plt.colorbar()
+
+    plt.figure('Dirty 3 imag')
+    plt.imshow(res3.imag/wsum)
+    plt.colorbar()
+
+    plt.show()
+
+
+    print(np.abs(res1.imag - res2.imag).max()/wsum)
+    print(np.abs(res1.real + res2.real).max()/wsum)
+
+
+    import ipdb; ipdb.set_trace()
+
+    # let us now take the real valued route in an attempt to mimick the gridder
     psfhat = jnp.fft.rfft2(iFs(psf),
                            s=(2*n, 2*n),
                            axes=(0, 1), norm='backward')
@@ -274,8 +340,6 @@ if __name__=="__main__":
     hat2 = Uhat - 1j*Vhat
     hat3 = Ihat - Qhat
 
-    # import ipdb; ipdb.set_trace()
-
     conv0 = jnp.fft.irfft2(hat0 * psfhat,
                           s=(2*n, 2*n),
                           axes=(0, 1),
@@ -301,76 +365,65 @@ if __name__=="__main__":
                           axes=(0, 1),
                           norm='backward')[0:n, 0:n]
     
-    Bconv = np.stack((conv0, conv1, conv2, conv3), axis=-1)/2
+    Bconv2 = np.stack((conv0, conv1, conv2, conv3), axis=-1)/2
     
-    dirtyI2, dirtyQ2, dirtyU2, dirtyV2 = corr_to_stokes(Bconv, wsum=wsum)
+    dirtyI3, dirtyQ3, dirtyU3, dirtyV3 = corr_to_stokes(Bconv2, wsum=wsum)
 
-    print(np.abs(dirtyI - dirtyI2).max())
-    print(np.abs(dirtyQ - dirtyQ2).max())
-    print(np.abs(dirtyU - dirtyU2).max())
-    print(np.abs(dirtyV - dirtyV2).max())
+    # compare Stokes dirty images
+    resI2 = np.abs(dirtyI - dirtyI3)
+    resQ2 = np.abs(dirtyQ - dirtyQ3)
+    resU2 = np.abs(dirtyU - dirtyU3)
+    resV2 = np.abs(dirtyV - dirtyV3)
 
+    assert np.allclose(resI2, 0, atol=epsilon)
+    assert np.allclose(resQ2, 0, atol=epsilon)
+    assert np.allclose(resU2, 0, atol=epsilon)
+    assert np.allclose(resV2, 0, atol=epsilon)
 
-    # import matplotlib.pyplot as plt
-    # plt.figure('1')
-    # plt.imshow(dirtyI2)
-    # plt.colorbar()
+    # compare corr dirty images
+    res0 = ID[:, :, 0] - Bconv2[:, :, 0]
+    res1 = ID[:, :, 1] - Bconv2[:, :, 1]
+    res2 = ID[:, :, 2] - Bconv2[:, :, 2]
+    res3 = ID[:, :, 3] - Bconv2[:, :, 3]
 
-    # plt.figure('2')
-    # plt.imshow(dirtyQ2)
-    # plt.colorbar()
-
-    # plt.figure('3')
-    # plt.imshow(dirtyU2)
-    # plt.colorbar()
-
-    # plt.figure('4')
-    # plt.imshow(dirtyV2)
-    # plt.colorbar()
-
-
-    # plt.show()
-
-    dd = ID - Bconv
     
-    
-    # Plot the dirty images
-    import matplotlib.pyplot as plt
     plt.figure('Dirty 0 real')
-    plt.imshow(dd[:, :, 0].real/wsum)
+    plt.imshow(res0.real/wsum)
     plt.colorbar()
 
     plt.figure('Dirty 0 imag')
-    plt.imshow(dd[:, :, 0].imag/wsum)
+    plt.imshow(res0.imag/wsum)
     plt.colorbar()
 
     plt.figure('Dirty 1 real')
-    plt.imshow(dd[:, :, 1].real/wsum)
+    plt.imshow(res1.real/wsum)
     plt.colorbar()
 
     plt.figure('Dirty 1 imag')
-    plt.imshow(dd[:, :, 1].imag/wsum)
+    plt.imshow(res1.imag/wsum)
     plt.colorbar()
 
     plt.figure('Dirty 2 real')
-    plt.imshow(dd[:, :, 2].real/wsum)
+    plt.imshow(res2.real/wsum)
     plt.colorbar()
 
     plt.figure('Dirty 2 imag')
-    plt.imshow(dd[:, :, 2].imag/wsum)
+    plt.imshow(res2.imag/wsum)
     plt.colorbar()
 
     plt.figure('Dirty 3 real')
-    plt.imshow(dd[:, :, 3].real/wsum)
+    plt.imshow(res3.real/wsum)
     plt.colorbar()
 
     plt.figure('Dirty 3 imag')
-    plt.imshow(dd[:, :, 3].imag/wsum)
+    plt.imshow(res3.imag/wsum)
     plt.colorbar()
 
     plt.show()
 
-    # Plot the
+    print(np.abs(res1.imag - res2.imag).max()/wsum)
+    print(np.abs(res1.real + res2.real).max()/wsum)
+
     import ipdb; ipdb.set_trace()
 
 
