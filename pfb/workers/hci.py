379889fd20b1846295bb0916c4230d89b42b3d0a
landmanbester <lbester@sarao.ac.za>
2025-09-12 09:35:33 +0200

    add OBSLABEL to fits header in stacked cube attributes

--- pfb/workers/hci.py
+++ pfb/workers/hci.py
@@ -614,6 +614,8 @@ def make_dummy_dataset(opts, utimes, freqs, radecs, time_mapping, freq_mapping,
     hdr['BTYPE'] = 'Intensity'
     hdr.update(wcs_hdr)
     hdr['TIMESCAL'] = delta_time
+    if opts.obslabel is not None:
+        hdr['OBSLABEL'] = opts.obs
 
     # if we don't pass these into stokes2im they get overwritten
     attrs={
