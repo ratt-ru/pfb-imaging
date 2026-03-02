fix units of PSF parameters to degrees

src/pfb_imaging/core/restore.py
@@ -21,7 +21,7 @@ def restore(
     residual_name: str = "RESIDUAL",
     suffix: str = "main",
     outputs: str = "mMrRiI",
-    gausspar: list[float] | None = None,
+    gausspar: tuple[float, float, float] | None = None,
     drop_bands: list[int] | None = None,
     nworkers: int = 1,
     nthreads: int | None = None,
@@ -109,15 +109,18 @@ def restore(
     nband = freqs.size
     ntime = timeids.size
     ncorr = dds[0].corr.size
-    cell_asec = dds[0].cell_rad * 180 / np.pi * 3600
-    log.info(f"Number of output times = {ntime}")
+    cell_deg = dds[0].cell_rad * 180 / np.pi
+    if ntime > 1:
+        log.info(f"Number of output times = {ntime}")
+        raise NotImplementedError("Multiple time ids not currently supported")
+    timeid = list(psfpars_mfs.keys())[0]
     log.info(f"Number of output bands = {nband}")
 
     if gausspar is None:
         gaussparf = None
-        gaussparf_mfs = None
+        gaussparf_mfs = psfpars_mfs[timeid]
         log.info("Using native resolution")
-    elif gausspar == [0, 0, 0]:
+    elif gausspar == (0, 0, 0):
         # This is just to figure out what resolution to convolve to
         dds = xds_from_list(dds_list, nthreads=nthreads, drop_all_but=["PSFPARSN"])
         emaj = 0.0
@@ -130,24 +133,25 @@ def restore(
             pas.append(np.mean(gausspars[:, 2]))
         pa = np.mean(np.array(pas))
         log.info(
-            f"Using lowest resolution of ({emaj * cell_asec:.3e} asec, "
-            f"{emin * cell_asec:.3e} asec, {pa:.3e} degrees) for restored images"
+            f"Using lowest resolution of ({emaj * cell_deg:.3e} deg, "
+            f"{emin * cell_deg:.3e} deg, {pa * 180 / np.pi:.3e} deg) for restored images"
         )
-        # these are n pixel units
+        # these are in pixel units
         gaussparf_mfs = [emaj, emin, pa]
-        gaussparf = gaussparf_mfs * ncorr
+        gaussparf = [gaussparf_mfs] * ncorr
     else:
+        # these are passed in in degrees
         gaussparf_mfs = list(gausspar)
         emaj = gaussparf_mfs[0]
         emin = gaussparf_mfs[1]
         pa = gaussparf_mfs[2]
         if "i" in outputs.lower():
-            log.info(
-                f"Using specified resolution of ({emaj:.3e} asec, {emin:.3e} asec, {pa:.3e} degrees) for restored image"
-            )
+            log.info(f"Using specified resolution of ({emaj:.3e} deg, {emin:.3e} deg, {pa:.3e} deg) for restored image")
         # convert to pixel units
         for i in range(2):
-            gaussparf_mfs[i] /= cell_asec
+            gaussparf_mfs[i] /= cell_deg
+        # convert to radians
+        gaussparf_mfs[2] *= np.pi / 180
         gaussparf = [gaussparf_mfs] * ncorr
 
     # create restored images
@@ -197,9 +201,14 @@ def restore(
         tasks.append(task)
 
     # we need to wait for tasksiI before rendering restored to fits
-    ray.get(tasksi)
+    tasksc = ray.get(tasksi)
 
     if "i" in outputs.lower():
+        # recompute the mean PSF parameters
+        psfparsf = np.zeros((nband, ncorr, 3))
+        for psfpar, bandid in tasksc:
+            psfparsf[int(bandid)] = psfpar
+        psfpars_mfs = psfparsf.mean(axis=0)
         task = rdds2fits.remote(
             dds_list,
             "IMAGE",
@@ -208,7 +217,8 @@ def restore(
             nthreads=nthreads,
             do_mfs="i" in outputs,
             do_cube="I" in outputs,
-            psfpars_mfs=psfpars_mfs,
+            psfpars_mfs={timeid: psfpars_mfs},
+            psfparsf=psfparsf,
             force_unit="Jy/beam",
         )
         tasks.append(task)

src/pfb_imaging/utils/fits.py
@@ -52,7 +52,19 @@ def save_fits(data, name, hdr, overwrite=True, dtype=np.float32, beams_hdu=None)
 
 
 def set_wcs(
-    cell_x, cell_y, nx, ny, radec, freq, unit="Jy/beam", gausspar=None, ms_time=None, header=True, casambm=True, ncorr=1
+    cell_x,
+    cell_y,
+    nx,
+    ny,
+    radec,
+    freq,
+    unit="Jy/beam",
+    gausspar=None,
+    gausspars=None,
+    ms_time=None,
+    header=True,
+    casambm=True,
+    ncorr=1,
 ):
     """
     cell_x/y - cell sizes in degrees
@@ -141,8 +153,8 @@ def set_wcs(
         if casambm:
             header["CASAMBM"] = casambm  # we need this to pick up the beams table
 
-        if gausspar is not None:
-            header = add_beampars(header, gausspar)
+        if gausspar is not None or gausspars is not None:
+            header = add_beampars(header, gausspar, gausspars=gausspars, unit2deg=cell_x)
 
         return header
     else:
@@ -161,9 +173,11 @@ def add_beampars(hdr, gausspar, gausspars=None, unit2deg=1.0):
     pfb/utils/misc/gaussian2d
 
     """
-    if len(gausspar) == 1:
+    if not isinstance(gausspar, np.ndarray):
+        gausspar = np.asarray(gausspar)
+    if len(gausspar.shape) == 2:
         gausspar = gausspar[0]
-    elif len(gausspar) != 3:
+    elif gausspar.shape[0] != 3:
         raise ValueError("Invalid value for gausspar")
 
     if not np.isnan(gausspar).any():
@@ -172,11 +186,15 @@ def add_beampars(hdr, gausspar, gausspars=None, unit2deg=1.0):
         hdr["BPA"] = gausspar[2] * 180 / np.pi
 
     if gausspars is not None:
-        for i in range(len(gausspars)):
+        gausspars = np.asarray(gausspars)
+        if len(gausspars.shape) != 2:
+            raise ValueError("gausspars should have shape (nband, 3)")
+        nband = gausspars.shape[0]
+        for i in range(nband):
             if not np.isnan(gausspars[i]).any():
-                hdr["BMAJ" + str(i + 1)] = gausspars[i][0] * unit2deg
-                hdr["BMIN" + str(i + 1)] = gausspars[i][1] * unit2deg
-                hdr["BPA" + str(i + 1)] = gausspars[i][2] * 180 / np.pi
+                hdr["BMAJ" + str(i + 1)] = gausspars[i, 0] * unit2deg
+                hdr["BMIN" + str(i + 1)] = gausspars[i, 1] * unit2deg
+                hdr["BPA" + str(i + 1)] = gausspars[i, 2] * 180 / np.pi
 
     return hdr
 
@@ -246,6 +264,7 @@ def dds2fits(
     do_mfs=True,
     do_cube=True,
     psfpars_mfs=None,
+    psfparsf=None,
     force_unit=None,
 ):
     basename = outname + "_" + column.lower()
@@ -278,8 +297,18 @@ def dds2fits(
         if do_mfs:
             # we need a single freq_mfs for the cube
             freq_mfs = np.sum(freqs[:, None] * wsums) / wsum.sum()
-            hdr = set_wcs(cell_deg, cell_deg, nx, ny, radec, freq_mfs, unit=unit, ms_time=dsb.time_out)
-            hdr["WSUM"] = wsum
+            hdr = set_wcs(
+                cell_deg,
+                cell_deg,
+                nx,
+                ny,
+                radec,
+                freq_mfs,
+                unit=unit,
+                ms_time=dsb.time_out,
+                gausspar=psfpars_mfs[dsb.timeid][0],
+            )  # always Stokes I in the header
+            hdr["WSUM"] = wsum[0]  # always Stokes I in the header
             if norm_wsum:
                 # already weighted by wsum
                 cube_mfs = np.sum(cube, axis=0) / wsum[:, None, None]
@@ -303,12 +332,38 @@ def dds2fits(
             if norm_wsum:
                 cube = cube / wsums[:, :, None, None]
             name = basename + f"_time{dsb.timeid}.fits"
-            hdr = set_wcs(cell_deg, cell_deg, nx, ny, radec, freqs, unit=unit, ms_time=dsb.time_out)
+            if psfparsf is None:
+                psfparsf = dsb.PSFPARSN if "PSFPARSN" in dsb else None
+            else:
+                psfparsf = np.asarray(psfparsf)
+                if len(psfparsf.shape) == 2:  # shape is (ncorr, 3)
+                    assert psfparsf.shape[0] == dsb.corr.size, "Number of corr in psfparsf does not match ncorr in dds"
+                    assert psfparsf.shape[1] == 3, "psfparsf should have shape (ncorr, 3)"
+                    psfparsf = np.tile(psfparsf[None, :, :], (nband, 1, 1))  # shape is (nband, ncorr, 3)
+                elif len(psfparsf.shape) == 1:  # shape is (3,)
+                    assert psfparsf.shape[0] == 3, "psfparsf should have shape (3,) or (ncorr, 3)"
+                    psfparsf = np.tile(psfparsf[None, None, :], (nband, dsb.corr.size, 1))  # shape is (nband, ncorr, 3)
+                psfparsf = xr.DataArray(
+                    data=psfparsf,
+                    coords={"band": np.arange(nband), "corr": dsb.corr.values, "bpar": dsb.bpar.values},
+                )
+            hdr = set_wcs(
+                cell_deg,
+                cell_deg,
+                nx,
+                ny,
+                radec,
+                freqs,
+                unit=unit,
+                ms_time=dsb.time_out,
+                gausspar=psfpars_mfs[dsb.timeid][0],  # always Stokes I in the header
+                gausspars=psfparsf.values[:, 0],
+            )  # always Stokes I in the header
             for i in range(nband):
                 hdr[f"WSUM{i + 1}"] = wsums[i, 0]  # always Stokes I value in fits header
 
-            if "PSFPARSN" in dsb:
-                beams_hdu = create_beams_table(dsb.PSFPARSN, cell2deg=cell_deg)
+            if psfparsf is not None:
+                beams_hdu = create_beams_table(psfparsf, cell2deg=cell_deg)
             else:
                 beams_hdu = None
             save_fits(cube, name, hdr, overwrite=True, dtype=otype, beams_hdu=beams_hdu)

src/pfb_imaging/utils/restoration.py
@@ -43,12 +43,15 @@ def restore_image(
     xx, yy = np.meshgrid(l_coord, m_coord, indexing="ij")
     residual = ds.get(residual_name).values / wsum[:, None, None]
 
-    if gaussparf is not None:
+    # units are (pixel, pixel, radians)
+    gausspari = ds.PSFPARSN.values
+
+    if gaussparf is not None:  # passed in in pixel units
         rconv = convolve2gaussres(
-            residual, xx, yy, gaussparf, nthreads=nthreads, gausspari=ds.PSFPARSN.values, pfrac=0.2, norm_kernel=False
+            residual, xx, yy, gaussparf, nthreads=nthreads, gausspari=gausspari, pfrac=0.2, norm_kernel=False
         )
     else:
-        gaussparf = ds.PSFPARSN.values
+        gaussparf = gausspari
         rconv = residual
     mconv = convolve2gaussres(model, xx, yy, gaussparf, nthreads=nthreads, pfrac=0.2, norm_kernel=False)
 
@@ -58,3 +61,4 @@ def restore_image(
     # only write updates
     ds = ds[["IMAGE", "PSFPARSF"]]
     ds.to_zarr(ds_name[0], mode="a")
+    return gaussparf, ds.bandid

src/pfb_imaging/utils/stokes2im.py
@@ -676,11 +676,13 @@ def stokes_image(
             dirty=psf[c],
         )
         # normalize by sum of weights to get Jy/beam units
-        # TODO - test with psf.max() when running sub-Nyquist
+        # done using psf_max in case some of the data points fell off the grid (sub-Nyquist imaging)
         for c in range(nstokes):
             if wsum[c] > 0:
-                psf[c] /= wsum[c]
-                residual[c] /= wsum[c]
+                psf_max = psf[c].max()
+                wsum[c] = psf_max
+                psf[c] /= psf_max
+                residual[c] /= psf_max
                 rms[c] = np.std(residual[c], axis=(0, 1))
 
     # these will be in degrees