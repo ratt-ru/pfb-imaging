da6054d69a32ab5cb950dacf2baef6d8fafd2ed6
landmanbester <lbester@sarao.ac.za>
2026-05-18 13:08:35 +0200

    fix: fix failing roundtrip tests

--- src/pfb_imaging/cli/hci.py
+++ src/pfb_imaging/cli/hci.py
@@ -457,8 +457,8 @@ def hci(
             rich_help_panel="Output",
         ),
         StimelaMeta(
-            mkdir=False,
             must_exist=False,
+            mkdir=False,
             path_policies={
                 "write_parent": True,
             },
