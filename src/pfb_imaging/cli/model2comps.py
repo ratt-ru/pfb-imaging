97c638b4348a660325d3956f92aed0d54ed98c28
landmanbester <lbester@sarao.ac.za>
2026-05-30 12:43:22 +0200

    fix: make numba-cache-dir an implicit output and hardcode it to /tmp/numba when running with container backend

--- src/pfb_imaging/cli/model2comps.py
+++ src/pfb_imaging/cli/model2comps.py
@@ -36,6 +36,15 @@ Directory = NewType("Directory", Path)
     path_policies={"write_parent": True},
     metadata={"rich_help_panel": "Output"},
 )
+@stimela_output(
+    dtype="Directory",
+    name="numba-cache-dir",
+    info="Directory to use for numba caching. Currently not configurable. Exists to ensure the directory is mounted.",
+    implicit="/tmp/numba",
+    must_exist=False,
+    mkdir=False,
+    path_policies={"write_parent": True},
+)
 def model2comps(
     output_filename: Annotated[
         str,
--- src/pfb_imaging/cli/restore.py
+++ src/pfb_imaging/cli/restore.py
@@ -49,6 +49,15 @@ File = NewType("File", Path)
     path_policies={"write_parent": True},
     metadata={"rich_help_panel": "Output"},
 )
+@stimela_output(
+    dtype="Directory",
+    name="numba-cache-dir",
+    info="Directory to use for numba caching. Currently not configurable. Exists to ensure the directory is mounted.",
+    implicit="/tmp/numba",
+    must_exist=False,
+    mkdir=False,
+    path_policies={"write_parent": True},
+)
 def restore(
     output_filename: Annotated[
         str,
