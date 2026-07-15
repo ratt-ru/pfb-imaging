340404d01d874356458a26b04af00a0a5ccec134
landmanbester <lbester@sarao.ac.za>
2026-07-15 09:23:03 +0200

    test(weighting): import as_contiguous_readonly_view from utils.weighting in test_weighting

--- tests/test_weighting.py
+++ tests/test_weighting.py
@@ -190,11 +190,11 @@ def test_box_sum_counts_3x3():
 
 
 def test_as_contiguous_readonly_view_flags():
-    from pfb_imaging.utils.stokes2im import _as_contiguous_readonly_view
+    from pfb_imaging.utils.weighting import as_contiguous_readonly_view
 
     # contiguous input: no copy, readonly view
     a = np.ones((4, 3, 2), dtype=np.float32)
-    v = _as_contiguous_readonly_view(a)
+    v = as_contiguous_readonly_view(a)
     assert v.flags["C_CONTIGUOUS"] and not v.flags["WRITEABLE"]
     assert np.shares_memory(a, v)
     assert a.flags["WRITEABLE"]  # original untouched
@@ -202,6 +202,6 @@ def test_as_contiguous_readonly_view_flags():
     # non-contiguous input (like the jones swapaxes view): copied contiguous
     b = np.ones((4, 3, 2), dtype=np.complex64).swapaxes(0, 1)
     assert not b.flags["C_CONTIGUOUS"]
-    w = _as_contiguous_readonly_view(b)
+    w = as_contiguous_readonly_view(b)
     assert w.flags["C_CONTIGUOUS"] and not w.flags["WRITEABLE"]
     assert not np.shares_memory(b, w)
